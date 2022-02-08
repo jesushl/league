4016335595362b85dee484313cbeee8fb0984541
jesushledon@gmail.com <jesushledon@gmail.com>
2022-02-08 13:54:48 -0600

    Moving files to make an easy deploy on docker

--- app/main.py
+++ app/main.py
@@ -3,12 +3,15 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 # Tools
-from matrix_opeations.matrix_ops import MatrixOperators
+from app.matrix_opeations.matrix_ops import MatrixOperators
 
 app = FastAPI()
 
+default__matrix_csv = "app/matrix.csv"
+
 @app.get("/echo")
-def echo(file: Optional[str]='matrix.csv'):
+def echo(file: Optional[str]=default__matrix_csv):
+    """Return a matrix representation in table."""
     try:
         mx_op = MatrixOperators(file)
         return HTMLResponse(content=mx_op.print(mx_op.matrix), status_code=200)
@@ -16,7 +19,8 @@ def echo(file: Optional[str]='matrix.csv'):
         return e
 
 @app.get("/invert")
-def  invert(file: Optional[str]='matrix.csv'):
+def  invert(file: Optional[str]=default__matrix_csv):
+    """Return a table with matrix transpose."""
     try:
         mx_op = MatrixOperators(file)
         return HTMLResponse(content=mx_op.print(mx_op.invert()), status_code=200)
@@ -24,7 +28,8 @@ def  invert(file: Optional[str]='matrix.csv'):
         return e
 
 @app.get("/flatten")
-def  flatten(file: Optional[str]='matrix.csv'):
+def  flatten(file: Optional[str]=default__matrix_csv):
+    """Rerturn a vector representation of matrix."""
     try:
         mx_op = MatrixOperators(file)
         return mx_op.flatten().tolist()
@@ -33,7 +38,8 @@ def  flatten(file: Optional[str]='matrix.csv'):
 
 
 @app.get("/sum")
-def  sum(file: Optional[str]='matrix.csv'):
+def  sum(file: Optional[str]=default__matrix_csv):
+    """Return cumulative sum of matrix."""
     try:
         mx_op = MatrixOperators(file)
         return mx_op.sum()
@@ -42,7 +48,8 @@ def  sum(file: Optional[str]='matrix.csv'):
 
 
 @app.get("/multiply")
-def  multiply(file: Optional[str]='matrix.csv'):
+def  multiply(file: Optional[str]=default__matrix_csv):
+    """Return of matrix values multiplication."""
     try:
         mx_op = MatrixOperators(file)
         return mx_op.multiply()
@@ -52,7 +59,8 @@ def  multiply(file: Optional[str]='matrix.csv'):
 
 @app.get("/")
 def  presentation():
-   return {
+    """HI."""
+    return {
        "name": "Jose de Jesus Herrera Ledon",
        "email": "jesushledon@gmail.com"
    } 
