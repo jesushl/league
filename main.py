32e8a5f204452e2a6c9dcf00464fb5faba637977
jesushledon@gmail.com <jesushledon@gmail.com>
2022-02-08 13:12:56 -0600

    removing debugin print

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def  invert(file: Optional[str]='matrix.csv'):
 def  flatten(file: Optional[str]='matrix.csv'):
     try:
         mx_op = MatrixOperators(file)
-        print(mx_op.flatten().tolist())
         return mx_op.flatten().tolist()
     except Exception as e:
         return e
